542ea46b422c1b00180cb4e4610af7fbc1ceba5f
rileywallaceriley <67809682+rileywallaceriley@users.noreply.github.com>
2024-02-26 12:11:58 -0500

    Update BBBlogger.py

--- BBBlogger.py
+++ BBBlogger.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def scrape_bbb_complaint_details(complaint_code):
     chrome_options = Options()
